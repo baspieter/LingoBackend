1b9479dd45e4323e93170e15c6256e329793bd67
baspieter <basvdveen2@hotmail.com>
2022-10-02 12:26:21 +0200

    Update GameWord.cs

--- Models/GameWord.cs
+++ Models/GameWord.cs
@@ -21,8 +21,8 @@ namespace Lingo.Models
       public Word Word { get; set; }
       [Required]
       public Game Game { get; set; }
-      public List<string> WordProgress { get; set; } = new();
-      public List<int> WordLetterProgress { get; set; } = new();
+      public List<string> WordProgress { get; set; } = new List<string>{ "", "", "", "", "" };
+      public List<int> WordLetterProgress { get; set; } = new List<int>{ 00000, 00000, 00000, 00000, 00000 };
       public bool Finished { get; set; } = false;
   }
 }
